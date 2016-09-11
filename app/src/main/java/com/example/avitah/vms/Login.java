da9b4a7409f65f1b24b0aa6310e91bf10ddc59fc
kaveer <kaveer.rajcoomar@hotmail.com>
2016-09-11 19:43:27 +0400

    link login and signup to main activity

--- app/src/main/java/com/example/avitah/vms/Login.java
+++ app/src/main/java/com/example/avitah/vms/Login.java
@@ -1,7 +1,9 @@
 package com.example.avitah.vms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public  void OnLoginButtonClick(View login){
+
+        if(login.getId() == R.id.ButtonLogin){
+            Intent i = new Intent(Login.this, VMS.class);
+            startActivity(i);
+        }
+    }
+
+    public void OnSignUpButtonClick(View signUp){
+        if(signUp.getId() == R.id.ButtonSignUp){
+            Intent i = new Intent(Login.this, SignUp.class);
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/avitah/vms/SignUp.java
+++ app/src/main/java/com/example/avitah/vms/SignUp.java
@@ -1,7 +1,9 @@
 package com.example.avitah.vms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void OnSignUpButtonClick(View signUp){
+        if(signUp.getId() == R.id.ButtonSignUp){
+            Intent i = new Intent(SignUp.this, VMS.class);
+            startActivity(i);
+        }
     }
 }
