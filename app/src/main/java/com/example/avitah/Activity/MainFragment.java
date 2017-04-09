5c0e43ef5eec5e601a83271b7ea67345ae71b45e
kaveer <kaveer.rajcoomar@gmail.com>
2017-04-09 21:41:43 +0400

    Change Dashboard

--- app/src/main/java/com/example/avitah/Activity/MainFragment.java
+++ app/src/main/java/com/example/avitah/Activity/MainFragment.java
@@ -8,40 +8,138 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.avitah.DbHandler.DBHandler;
+import com.example.avitah.Tables.TableUser;
+import com.example.avitah.Tables.TableVehicle;
 import com.example.avitah.vms.R;
+
+import java.util.ArrayList;
 
 
 public class MainFragment extends Fragment {
 
-    ListView listView1;
-    ListView listView2;
-    ListView listView3;
+    ListView listViewVehicle;
+   // ListView listViewOwnerDetails;
+    ListView listViewAnnualExp;
+    ListView listViewExp;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    TextView ownerDetails;
+
+    String fuel = "Total Fuel expenses :";
+    String repairAndServicing = "Total repair and servicing expenses :";
+    String fine = "Total fine expenses :";
+    String carWash = "Total car wash expenses";
+    String parking = "Total parking expenses :";
+    String otherExpenses = "Total Other expenses :";
+
+    String insurance = "Total insurance expenses :";
+    String fitness = "Total fitness expenses :";
+    String roadTaxation = "Total road taxation expenses :";
+
+    ArrayList<TableVehicle.VehicleNonStatic> vehicleList;
+    String[] vehicleArray;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getActivity().setTitle("Dashboard");
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        InitializeListView(view);
+
+        ownerDetails.setText("Name: " + TableUser.firstName + "\t" + TableUser.lastName);
+        GetVehicle();
+        GetExpenseReport();
+        GetAnnualExpenseReport();
+        GenerateListView();
+
+        return view;
+    }
+
+    public void GetVehicle(){
+        TableVehicle.userId = TableUser.userId;
+
+        DBHandler DB = new DBHandler(getContext());
+        vehicleList = DB.GetVehicle();
+
+        if(vehicleList.isEmpty()){
+            vehicleArray = new String[1];
+            vehicleArray[0] = "No vehicle Recorded";
+        }
+        else {
+            GenerateVehicleList();
+        }
+    }
+
+    private void GenerateVehicleList() {
+        vehicleArray = new String[vehicleList.size()];
+        int counter = 0;
+        for (TableVehicle.VehicleNonStatic vehicle:  vehicleList) {
+            vehicleArray[counter] = vehicle.make + "\t" + vehicle.model;
+            counter ++;
+        }
+    }
+
+    private void GetAnnualExpenseReport() {
+        DBHandler DB = new DBHandler(getContext());
+
+        insurance = "Total insurance expenses :" + DB.GetInsuranceExpense(TableUser.userId);
+        fitness = "Total fitness expenses :" + DB.GetFitnessExpense(TableUser.userId);
+        roadTaxation = "Total road taxation expenses :" + DB.GetRoadTaxationExpense(TableUser.userId);
+    }
+
+    private void InitializeListView(View view) {
+        listViewVehicle = (ListView)view.findViewById(R.id.listViewVehicle);
+       // listViewOwnerDetails = (ListView)view.findViewById(R.id.listViewOwner);
+        listViewAnnualExp = (ListView)view.findViewById(R.id.listViewAnnualExp);
+        listViewExp = (ListView)view.findViewById(R.id.listViewExp);
+
+        ownerDetails = (TextView) view.findViewById(R.id.ownerDetails);
+    }
+
+    public void GenerateListView(){
+
+
+        String[] valuesExp = new String[] { fuel, repairAndServicing, fine, carWash, parking, otherExpenses };
+        String[] valuesAnnualExp = new String[] { insurance, fitness, roadTaxation };
+
+        ArrayAdapter adapterExp = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, valuesExp);
+        ArrayAdapter adapterAnnualExp = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, valuesAnnualExp);
+        ArrayAdapter adapterVehicle = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, vehicleArray);
+
+
+        listViewVehicle.setAdapter(adapterVehicle);
+        //listViewOwnerDetails.setAdapter(adapterExp);
+        listViewAnnualExp.setAdapter(adapterAnnualExp);
+        listViewExp.setAdapter(adapterExp);
+
+    }
+
+    private void GetExpenseReport() {
+        DBHandler DB = new DBHandler(getContext());
+
+        fuel = "Total Fuel expenses :" + DB.GetFuelExpense(TableUser.userId);
+        repairAndServicing = "Total repair and servicing expenses :" + DB.GetRepairAndServicingExpense(TableUser.userId);
+        fine = "Total fine expenses :" + DB.GetFineExpense(TableUser.userId);
+        carWash = "Total car wash expenses" + DB.GetCarWashExpenses(TableUser.userId);
+        parking = "Total parking expenses :" + DB.GetParkingExpense(TableUser.userId);
+        otherExpenses = "Total Other expenses :" + DB.GetOtherExpenses(TableUser.userId);
+    }
+
+    //==================== GENERATED METHOD ===========================================
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainFragment newInstance(String param1, String param2) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
@@ -58,95 +156,5 @@ public class MainFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
-
-
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-        listView1 = (ListView)view.findViewById(R.id.list1);
-        listView2 = (ListView)view.findViewById(R.id.list2);
-
-        listView3 = (ListView)view.findViewById(R.id.list3);
-
-
-        String[] values = new String[] { "Android List View",
-                "Adapter implementation ",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View"
-        };
-
-        GenerateListView(values);
-
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view,
-//                                    int position, long id) {
-//
-//                // ListView Clicked item index
-//                int itemPosition     = position;
-//
-//                // ListView Clicked item value
-//                String  itemValue    = (String) listView.getItemAtPosition(position);
-//
-//                // Show Alert
-//
-//                Toast messageBox = Toast.makeText(getActivity() , +itemPosition+ "Insurance removed" +itemValue  , Toast.LENGTH_SHORT);
-//                messageBox.show();
-//
-//
-//            }
-//
-//        });
-
-        return view;
-    }
-
-    public void GenerateListView(String[] values){
-        ArrayAdapter adapter = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, values);
-        listView1.setAdapter(adapter);
-        listView2.setAdapter(adapter);
-
-        listView3.setAdapter(adapter);
-
-    }
-
-
-
-
-
 }
